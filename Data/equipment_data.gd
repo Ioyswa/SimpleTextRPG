1046450b93d98ee084c627713e8267ba35befc2f
Ioyswa <HangFTane123@gmail.com>
2025-05-05 18:11:20 +0700

    add starter gear after player select class

---
+++ Data/equipment_data.gd
@@ -0,0 +1,145 @@
+extends Node
+
+var starter_gear = {
+	"Warrior": {
+		"Helmet": {
+			1: {
+				"Name": "Wooden Helmet",
+				"Stats": {
+					"Health": 50,
+					"Defense": 20
+				}
+			},
+		},
+		"Chestplate": {
+			1: {
+				"Name": "Wooden Chestplate",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Legging": {
+			1: {
+				"Name": "Wooden Legging",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Boots": {
+			1: {
+				"Name": "Wooden Boots",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Sword": {
+			0 : {
+				"Name" : "Wooden Sword",
+				"Stats" : {
+					"Attack" : 5,
+					"Str" : 20,
+				}
+			}
+		},
+	},
+	"Archer": {
+		"Helmet": {
+			1: {
+				"Name": "Wooden Helmet",
+				"Stats": {
+					"Health": 50,
+					"Defense": 20
+				}
+			},
+		},
+		"Chestplate": {
+			1: {
+				"Name": "Wooden Chestplate",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Legging": {
+			1: {
+				"Name": "Wooden Legging",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Boots": {
+			1: {
+				"Name": "Wooden Boots",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Bow": {
+			0 : {
+				"Name" : "Wooden Bow",
+				"Stats" : {
+					"Attack" : 5,
+					"Str" : 20,
+				}
+			}
+		},
+	},
+	"Mage": {
+		"Helmet": {
+			1: {
+				"Name": "Wooden Helmet",
+				"Stats": {
+					"Health": 50,
+					"Defense": 20
+				}
+			},
+		},
+		"Chestplate": {
+			1: {
+				"Name": "Wooden Chestplate",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Legging": {
+			1: {
+				"Name": "Wooden Legging",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Boots": {
+			1: {
+				"Name": "Wooden Boots",
+				"Stats": {
+					"Health": 150,
+					"Defense": 50,
+				}
+			}
+		},
+		"Staff": {
+			0 : {
+				"Name" : "Wooden Staff",
+				"Stats" : {
+					"Attack" : 5,
+					"Str" : 20,
+				}
+			}
+		},
+	},
+}
